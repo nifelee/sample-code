e788061fc72c08f2afe42315a197c719acb5a72e
nifelee <nifelee@gmail.com>
2020-12-11 09:52:15 +0900

    init

---
+++ rabbitmq-amqp/src/main/java/com/nifelee/rabbitmqamqp/RabbitmqAmqpApplication.java
@@ -0,0 +1,13 @@
+package com.nifelee.rabbitmqamqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqAmqpApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RabbitmqAmqpApplication.class, args);
+  }
+
+}
